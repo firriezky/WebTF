fcf379b6ffa3cedbed0ebd10b1a309c39bc20439
henryaugusta <henryaugusta4@gmail.com>
2021-01-10 16:25:06 +0700

    add route for calling artisan command

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,6 +17,24 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Symlink !important pisan atuh nih
+Route::get('/command/foo', function () {
+    Artisan::call('storage:link');
+});
+
+Route::get('/command/migrate_reset', function () {
+    Artisan::call('migrate:reset');
+});
+
+Route::get('/command/migrate', function () {
+    Artisan::call('migrate');
+});
+
+Route::get('/command/seeder', function () {
+    Artisan::call('db:seed');
+});
+
 
 
 
